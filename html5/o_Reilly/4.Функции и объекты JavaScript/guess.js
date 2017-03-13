c041595f06f757bffe149f669a10e7a7282c0bde
Alexander Kokhan <alexander_kohan@outlook.com>
2017-03-13 23:02:31 +0200

    chap4

--- html5/o_Reilly/4.Функции и объекты JavaScript/guess.js
+++ html5/o_Reilly/4.Функции и объекты JavaScript/guess.js
@@ -1,17 +1,19 @@
-function checkGuess(guess){
-	var answers = ["red",
-	"green",
-	"blue",];
-	
-	var index = Math.random() *answers.length);
-	
-	if (guess == answers[index]){
-		answer = "You're right! I was thinking of " + answers[index];
-	} else{
-		answer = "Sorry, I was thinking of " + answers[index];
-	}
-	
-	return answer;
+
+
+function checkGuess(guess) {
+    var answers = ["red",
+        "green",
+        "blue",];
+
+    var index = Math.random() * answers.length;
+
+    if (guess == answers[index]) {
+        answer = "You're right! I was thinking of " + answers[index];
+    } else {
+        answer = "Sorry, I was thinking of " + answers[index];
+    }
+
+    return answer;
 }
 
-checkGuess("red");
+console.log(checkGuess("red"));
--- html5/o_Reilly/4.Функции и объекты JavaScript/webvilleCinema.js
+++ html5/o_Reilly/4.Функции и объекты JavaScript/webvilleCinema.js
@@ -1,13 +1,39 @@
-var movie1 ={
-	title: "Plan 9 from Outer Space",
-	genre:"Cult Classic",
-	rating: 5,
-	showtim.es: ["3:00pm", "7:00рт", "11:0 Орт"]
+var movie1 = {
+    title: "Plan 9 from Outer Space",
+    genre: "Cult Classic",
+    rating: 5,
+    showTimes: ["15:00", "19:00", "23:00"]
 }
 
 var movie2 = {
-	title:"Forbidden Planet",
-	genre: "Classic Sci-fi",
-	rating: 5,
-	showtimes: ["5:00pm", "9:00pm"]
+    title: "Forbidden Planet",
+    genre: "Classic Sci-fi",
+    rating: 5,
+    showTimes: ["17:00", "21:00"]
 }
+
+function getNextShowing(movie) {
+    var now = new Date().getTime();
+
+    for (var i = 0; i < movie.showTimes.length; i++) {
+        var showTimes = getTimeFromString(movie.showTimes[i]);
+        if ((showTimes - now) > 0) {
+            return "Next showing of " + movie.title + " is " + movie.showTimes[i];
+        }
+    }
+    return "Sessions are no longer available today" ;
+}
+
+function getTimeFromString(timeString){
+    var theTime = new Date();
+    var time = timeString.match(/(\d+)(?::(\d\d))?\s*(p?)/);
+    theTime.setHours(parseInt(time[1]) + (time[3] ? 12 : 0));
+    theTime.setMinutes( parseInt(time[2]) || 0 ) ;
+    return theTime.getTime();
+}
+
+var nextShowing = getNextShowing(movie1);
+console.log(nextShowing);
+
+nextShowing = getNextShowing(movie2);
+console.log(nextShowing);
